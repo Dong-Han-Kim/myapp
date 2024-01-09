a0b8d6a19a6de4327e63e9538d1eed13e7ccca34
Dong-Han-Kim <dh32110@gmail.com>
2024-01-09 21:21:16 +0900

    about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,4 +1,6 @@
+import { Next, GitHub } from '@/app/_asset/icons';
 import styles from './about.module.css';
+import Image from 'next/image';
 
 export default function About() {
 	return (
@@ -10,13 +12,45 @@ export default function About() {
 			</div>
 			<div className={styles.container}>
 				<div className={styles.about}>
-					<h3>ABOUT ME</h3>
+					<h3 className={styles.categories}>ABOUT ME</h3>
+					<p>안녕하세요. 프론트엔드 개발자가 되고 싶은 김동한입니다. 방문해 주셔서 감사합니다.</p>
 				</div>
 				<div className={styles.education}>
-					<h3>EDUCATION</h3>
+					<h3 className={styles.categories}>EDUCATION</h3>
+					<ul>
+						<li>
+							<p>중원대학교 경찰행정학과</p>
+							<p className={styles.date}>2011.02 ~ 2018.02</p>
+						</li>
+						<li>
+							<p>충청 ICT 취·창업 역량강화 프로그램</p>
+							<p className={styles.date}>2023.10 ~ 2023.11</p>
+						</li>
+					</ul>
 				</div>
 				<div className={styles.skill}>
-					<h3>SKILL</h3>
+					<h3 className={styles.categories}>SKILL</h3>
+					<div>
+						<h5 className={styles.subTitle}>Basic</h5>
+						<div className={styles.skillBox}>
+							<Image src={'/HTML5.svg.png'} width={50} height={50} alt="HTML" />
+							<Image src={'/CSS.svg.png'} width={50} height={50} alt="CSS" />
+							<Image src={'/JavaScript.svg.png'} width={50} height={50} alt="JavaScript" />
+						</div>
+						<h5 className={styles.subTitle}>Library</h5>
+						<div className={styles.skillBox}>
+							<Image src={'/React.svg.png'} width={50} height={50} alt="React" />
+						</div>
+						<h5 className={styles.subTitle}>Framework</h5>
+						<div className={styles.skillBox}>
+							<Next />
+							<Image src={'/Tailwind.svg.png'} width={150} height={50} alt="Tailwind-css" />
+						</div>
+						<h5 className={styles.subTitle}>etc</h5>
+						<div className={styles.skillBox}>
+							<GitHub />
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { useRef } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+	const textRef = useRef<HTMLDivElement>(null);
+	console.log(textRef.current);
+
 	return (
 		<main>
 			<div className={styles.titleBox}>
@@ -9,7 +15,12 @@ export default function Home() {
 				</h1>
 			</div>
 			<div className={styles.container}>
-				<h1>KIM DONGHAN</h1>
+				<h1 className={styles.myName}>KIM DONGHAN</h1>
+				<div className={styles.changeTextBox} ref={textRef}>
+					<div className={styles.isActive}>Challenge</div>
+					<div className={styles.isHidden}>Front-end</div>
+					<div className={styles.isHidden}>Developer</div>
+				</div>
 			</div>
 		</main>
 	);
