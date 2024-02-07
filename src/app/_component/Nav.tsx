00935d4f1110e39b691161e06361fc5267cec00e
Dong-Han-Kim <dh32110@gmail.com>
2024-02-07 10:03:08 +0900

    test

--- src/app/_component/Nav.tsx
+++ src/app/_component/Nav.tsx
@@ -5,15 +5,24 @@ import styles from './Nav.module.css';
 import { MenuBar } from '../_asset/icons';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Nav() {
+	const [menu, setMenu] = useState(false);
 	const segment = useSelectedLayoutSegment();
 	const toggleRef = useRef<HTMLUListElement>(null);
 
 	const toggleMenu = () => {
 		if (toggleRef.current) {
 			toggleRef.current.classList.toggle(styles.toggleMenu);
+			setMenu(true);
+		}
+	};
+
+	const listHandler = () => {
+		if (toggleRef.current && menu) {
+			toggleRef.current.classList.toggle(styles.toggleMenu);
+			setMenu(false);
 		}
 	};
 
@@ -24,7 +33,7 @@ export default function Nav() {
 					<Image src={'/logo.png'} width={100} height={100} alt="Logo" />
 				</Link>
 			</div>
-			<ul className={styles.items} ref={toggleRef}>
+			<ul className={styles.items} ref={toggleRef} onClick={listHandler}>
 				{segment === null ? (
 					<Link href="/">
 						<li style={{ backgroundColor: 'rgba(0, 0, 0)', color: 'white' }}>Home</li>
