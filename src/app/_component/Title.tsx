bf06ca934a5e73fa873270943a5751deeef34c3b
Dong-Han-Kim <dh32110@gmail.com>
2024-06-07 14:52:00 +0900

    test

--- src/app/_component/Title.tsx
+++ src/app/_component/Title.tsx
@@ -8,38 +8,38 @@ export default function Title() {
 	console.log(pathname);
 
 	return (
-		<main className={style.main}>
-			<div className={style.changeTitle}>
-				{pathname === '/' ? (
-					<div className={style.titleBox}>
-						<h1 className={style.title}>HOME</h1>
-					</div>
-				) : null}
+		// <div className={style.main}>
+		<div className={style.changeTitle}>
+			{pathname === '/' ? (
+				<div className={style.titleBox}>
+					<h1 className={style.title}>HOME</h1>
+				</div>
+			) : null}
 
-				{pathname === '/about' ? (
-					<div className={style.titleBox}>
-						<h1 className={style.title}>ABOUT</h1>
-					</div>
-				) : null}
+			{pathname === '/about' ? (
+				<div className={style.titleBox}>
+					<h1 className={style.title}>ABOUT</h1>
+				</div>
+			) : null}
 
-				{pathname === '/project' ? (
-					<div className={style.titleBox}>
-						<h1 className={style.title}>PROJECT</h1>
-					</div>
-				) : null}
+			{pathname === '/project' ? (
+				<div className={style.titleBox}>
+					<h1 className={style.title}>PROJECT</h1>
+				</div>
+			) : null}
 
-				{pathname === '/contact' ? (
-					<div className={style.titleBox}>
-						<h1 className={style.title}>CONTACT</h1>
-					</div>
-				) : null}
+			{pathname === '/contact' ? (
+				<div className={style.titleBox}>
+					<h1 className={style.title}>CONTACT</h1>
+				</div>
+			) : null}
 
-				{pathname === '/comment' ? (
-					<div className={style.titleBox}>
-						<h1 className={style.title}>Comment</h1>
-					</div>
-				) : null}
-			</div>
-		</main>
+			{pathname === '/comment' ? (
+				<div className={style.titleBox}>
+					<h1 className={style.title}>Comment</h1>
+				</div>
+			) : null}
+		</div>
+		// </div>
 	);
 }
